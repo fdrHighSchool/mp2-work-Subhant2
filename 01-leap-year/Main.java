e0920b1c1af668d62286c5b565291c7c698a51dc
Subhant2 <114924005+Subhant2@users.noreply.github.com>
2022-12-01 08:13:53 -0500

    Create Main.java

---
+++ 01-leap-year/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+   public static void main(String[] args) {
+      int year;
+      System.out.print("Enter an Year: ");
+      Scanner sc = new Scanner(System.in);
+      year = sc.nextInt();
+
+      if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+         System.out.println("This is a leap year");
+      else
+         System.out.println("This is not a leap year");
+   }
+}
